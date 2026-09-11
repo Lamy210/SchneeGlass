9ff9d4792745ebcd32a77d8fd18dc73dc40bdec9
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-11 09:30:41 +0900

    fix: include drop source limit in rejection

--- Packages/SchneeGlassKit/Sources/SchneeGlassFileSystemAdapter/NativeDropPlanningAdapter.swift
+++ Packages/SchneeGlassKit/Sources/SchneeGlassFileSystemAdapter/NativeDropPlanningAdapter.swift
@@ -251,7 +251,7 @@ public actor NativeDropPlanningAdapter: DropPlanning {
         destinationAccess: FolderAccessHandle
     ) async -> DropPlan {
         guard sourceURLs.count <= Self.maximumSourceItemsPerPlan else {
-            return .reject(.tooManyItems)
+            return .reject(.tooManyItems(maximum: Self.maximumSourceItemsPerPlan))
         }
 
         return await makePlan(
@@ -267,7 +267,7 @@ public actor NativeDropPlanningAdapter: DropPlanning {
         destinationAccess: FolderAccessHandle
     ) async -> DropPlan {
         guard sourceURLs.count <= Self.maximumSourceItemsPerPlan else {
-            return .reject(.tooManyItems)
+            return .reject(.tooManyItems(maximum: Self.maximumSourceItemsPerPlan))
         }
 
         return await makePlan(
