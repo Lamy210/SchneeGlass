7b3e02d8132dc60a678744f9082e63624a0148d1
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-12 04:46:53 +0900

    refactor: require abandonable runtime planner

--- Packages/SchneeGlassKit/Sources/SchneeGlassApplication/GlassRuntimeSessionFactory.swift
+++ Packages/SchneeGlassKit/Sources/SchneeGlassApplication/GlassRuntimeSessionFactory.swift
@@ -2,23 +2,20 @@ public struct GlassRuntimeSessionFactory: Sendable {
     private let eventStreaming: any FileEventStreaming
     private let snapshotReader: any FolderSnapshotReading
     private let accessController: any FolderAccessControlling
-    private let dropPlanning: any DropPlanning
-    private let copyAbandoner: any AuthorizedCopyBatchAbandoning
+    private let dropPlanning: any DropPlanning & AuthorizedCopyBatchAbandoning
     private let fileCopying: any FileCopying
 
     public init(
         eventStreaming: any FileEventStreaming,
         snapshotReader: any FolderSnapshotReading,
         accessController: any FolderAccessControlling,
-        dropPlanning: any DropPlanning,
-        copyAbandoner: any AuthorizedCopyBatchAbandoning,
+        dropPlanning: any DropPlanning & AuthorizedCopyBatchAbandoning,
         fileCopying: any FileCopying
     ) {
         self.eventStreaming = eventStreaming
         self.snapshotReader = snapshotReader
         self.accessController = accessController
         self.dropPlanning = dropPlanning
-        self.copyAbandoner = copyAbandoner
         self.fileCopying = fileCopying
     }
 
@@ -29,7 +26,6 @@ public struct GlassRuntimeSessionFactory: Sendable {
             snapshotReader: snapshotReader,
             accessController: accessController,
             dropPlanning: dropPlanning,
-            copyAbandoner: copyAbandoner,
             fileCopying: fileCopying
         )
     }
