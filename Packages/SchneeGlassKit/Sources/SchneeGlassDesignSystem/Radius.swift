e37262ecfcb7577669bd7d3bd616d7e1fa35e591
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-12 18:21:04 +0900

    refactor: add semantic radius tokens

---
+++ Packages/SchneeGlassKit/Sources/SchneeGlassDesignSystem/Radius.swift
@@ -0,0 +1,10 @@
+#if canImport(SwiftUI)
+import SwiftUI
+
+public enum SchneeGlassRadius {
+    public static let glassSurface: CGFloat = 20
+    public static let messageBanner: CGFloat = 12
+    public static let workspaceDropOverlay: CGFloat = 18
+    public static let desktopDropOverlay: CGFloat = 14
+}
+#endif
